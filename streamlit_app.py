87e6a0b175f7170e62f5556f09f0d17bd55c8017
Piyara Perera <piyaraperera04@gmail.com>
2026-04-28 02:04:14 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="wide"
 )
 
-df = pd.read_csv("data/cleaned/cme_cleaned.csv")
+df = pd.read_csv("cme_cleaned.csv")
 
 st.title("Child Mortality Analysis Dashboard")
 st.markdown(
